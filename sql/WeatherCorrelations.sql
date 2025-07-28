cf588ef780ab4faacad62fb435e2fe2fe4b53fce
Zach Richardson <zachrichardson1114@gmail.com>
2025-07-28 06:48:10 -0400

    added crime rates per capita by neighborhood

--- sql/WeatherCorrelations.sql
+++ sql/WeatherCorrelations.sql
@@ -1,4 +1,5 @@
 /* WEATHER-CRIME CORRELATIONS */
+select offense_category_id from crimes;
 SELECT *
 	FROM weather w
 		INNER JOIN crimes cr
@@ -32,3 +33,28 @@ SELECT total.icon conditions,
 /*
 	Do aggrevated assaults occur more frequently on days with extreme temperatures?
 */
+
+SELECT temp_categories.category, COUNT(agg_assaults.*) assault_count
+	FROM (
+		SELECT w.temp temperature
+		FROM weather w INNER JOIN crimes cr
+			ON w.datetime::Date = cr.first_occurrence_date::Date
+		WHERE cr.offense_category_id = 'aggravated-assault'
+	) agg_assaults INNER JOIN (
+		SELECT 'COLD DAYS' category, -100 low_threshold, 31.999 high_threshold
+		UNION ALL
+		SELECT 'MILD DAYS' category, 32.0 low_threshold, 79.999 high_threshold
+		UNION ALL
+		SELECT 'HOT DAYS' category, 80.0 low_threshold, 200 high_threshold 
+	) temp_categories
+		ON agg_assaults.temperature BETWEEN temp_categories.low_threshold AND temp_categories.high_threshold
+	GROUP BY temp_categories.category;
+
+SELECT w.temp temperature, cr.offense_category_id, w.datetime::Date
+	FROM weather w INNER JOIN crimes cr
+		ON w.datetime::Date = cr.first_occurrence_date::Date
+	WHERE cr.offense_category_id = 'aggravated-assault'
+		AND w.temp NOT BETWEEN -100 and 200
+	ORDER BY temperature ASC;
+
+SELECT COUNT(*) from crimes where offense_category_id = 'aggravated-assault';
